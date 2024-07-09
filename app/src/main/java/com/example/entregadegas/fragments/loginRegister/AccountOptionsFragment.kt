a7880cc8d6663fd8ad3a7138567a68014ef844ec
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2024-07-09 17:35:12 -0300

    09.07.2024 -> Adiciona recursos e cria o fragment_introduction.xml

---
+++ app/src/main/java/com/example/entregadegas/fragments/loginRegister/AccountOptionsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.entregadegas.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.entregadegas.R
+
+class AccountOptionsFragment: Fragment(R.layout.fragment_account_options) {
+}
